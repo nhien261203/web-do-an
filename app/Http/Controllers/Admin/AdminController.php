19626176e3df0e192f67808f7455197bd00a422a
duogbachdev <contact.duogbachdev@gmail.com>
2024-07-07 21:41:43 +0700

    End

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    //
+    // public function index(Request $request)
+    // {
+    //     return view('Admin.dashboard.index');
+    // }
+
+    public function logout()
+    {
+        // $request->session()->forget("email");
+        Auth::logout();
+        return redirect("/login");
+    }
+}
---
+++ app/Http/Controllers/Admin/Auth/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function getLogin()
+    {
+        return view('Admin/login');
+    }
+    public function postLogin(LoginRequest $request)
+    {
+        // $rules = [
+        //     "email" => "required|email",
+        //     "password" => "required|min:3|max:6",
+        // ];
+        // $messages = [
+        //     "email.required" => "Email không được để trống",
+        //     "email.email" => "Email không hợp lệ",
+        //     "password.required" => "Password không được để trống",
+        //     "password.min" => "Password tối thiểu là 3 ký tự",
+        //     "password.max" => "Password tối đa là 6 ký tự",
+        // ];
+
+        // $request->validate($rules, $messages);
+        // $users = DB::table("users")->where("email", $request->email)->where("password", $request->password)->get()->all();
+
+        // if (count($users) > 0) {
+        if (Auth::attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ])) {
+            // $request->session()->put("email", $request->email);
+            return redirect("quantri/dashboard");
+        } else {
+            return redirect()->back()->withErrors("Tài khoản không hợp lệ");
+        }
+        // return view('backend/login');
+    }
+}
